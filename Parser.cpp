fc12411971c536e1bd821cc94dbb976643384ab7
Degen Laurent <laurent.degen@supelec.fr>
2015-11-05 12:04:00 +0100

    start parser implementation

--- Parser.cpp
+++ Parser.cpp
@@ -3,7 +3,36 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <iostream>
 
 std::vector<fuzzy::Regle> fuzzy::Parser::parse(std::string filename){
-  return tableau_regle;
+  std::ifstream fichier("regles.txt");
+  if(fichier){
+    std::string nom;
+    std::string prem_p1, prem_p2;
+    std::vector< std::pair<std::string, std::string> > premisses;
+    std::pair<std::string, std::string> conclusion;
+    bool est_floue;
+    float coeff;
+    
+    std::getline(fichier,nom);
+    fichier>>prem_p1;
+    while(prem_p1!="Alors"){
+      fichier>>prem_p1;
+      fichier>>prem_p2;
+      premisses.push_back(std::pair<std::string, std::string>(prem_p1, prem_p2));
+    }
+    fichier>>prem_p1;
+    fichier>>prem_p2;
+    conclusion= std::pair<std::string, std::string>(prem_p1, prem_p2);
+    fichier>>prem_p1;
+    fichier>>coeff;
+    est_floue=true;
+
+    tableau_regles.push_back(Regle(nom, premisses, conclusion, est_floue, coeff));
+  }
+  else{
+    std::cout<<"Erreur: fichier impossible à ouvrir"<<std::endl;
+  }
+  return tableau_regles;
 }
