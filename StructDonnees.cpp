e94eb7dc4720d1862f89e61b0b8e29fb33023ba3
Degen Laurent <laurent.degen@supelec.fr>
2015-12-10 13:57:56 +0100

    pb merge résolu!

--- StructDonnees.cpp
+++ StructDonnees.cpp
@@ -5,11 +5,16 @@
 
 
 namespace fuzzy {
+
+  ///////LINGVAR////////
   LingVar::LingVar() {
     std::cout << "Initialisation vide de LingVar." << std::endl;
   }
   LingVar::LingVar(std::string n, std::string d) : name(n), description(d) {}
-  LingVar::LingVar(std::string n, std::string d, std::vector<float> vk, std::vector<float> vv) : name(n), description(d), vec_keys(vk), vec_values(vv) {}
+
+  LingVar::LingVar(std::string n, std::string d, std::vector<float> vk, std::vector<float> vv) 
+    : name(n), description(d), vec_keys(vk), vec_values(vv) {}
+
   LingVar::~LingVar() {}
 
   float LingVar::getMembershipValue(float var_val) {
@@ -30,73 +35,49 @@ namespace fuzzy {
     return result;
   }
 
-        
-  Regle::Regle(std::string n, std::vector<std::shared_ptr<Fait> > p, std::vector<std::shared_ptr<Fait> >  c, bool f, float cf) : nom(n), flou(f), coeff(cf) {
+  ////////REGLES/////////
+  Regle::Regle(std::string n, std::vector<std::shared_ptr<Fait> > p, std::vector<std::shared_ptr<Fait> >  c, bool f, float cf) 
+    : nom(n), flou(f), coeff(cf) {} //constructeur ave argumets
+
+  Regle::~Regle() {} //destructeur par défaut
+
+  ///////BaseRegles/////////
+  BaseRegles::BaseRegles() {} //constructeur pa défaut
+
+  BaseRegles::BaseRegles(const BaseRegles& br) : data(br.getData()) {}//constucteur par recopie
+
+  BaseRegles& BaseRegles::operator=(const BaseRegles& br) { //surcharge de l'opérateur =
+    data = br.getData();
+    return *this;
   }
-  Regle::~Regle() {}
-	Regle::Regle(std::string n, std::vector<std::pair<std::string, std::string> > p, std::pair<std::string, std::string> c, bool f, float cf) : nom(n), flou(f), coeff(cf) {
-	}
-	Regle::~Regle() {}
+
+  BaseRegles::BaseRegles(BaseRegles&& br) : data(br.getData()) {} //constructeur par copie de référence
+
+  BaseRegles& BaseRegles::operator=(BaseRegles&& br) { ////surcharge de l'opérateur = par référence
+    data = br.getData();
+    return *this;
+  }
+  BaseRegles::~BaseRegles() {} //destructeur par défaut
+
+  void BaseRegles::rmRegle(std::string regle) {
+    data.erase(regle);
+  }
 
 
-	BaseRegles::BaseRegles() {}
-	BaseRegles::BaseRegles(std::vector<Regle> regles) {
-		for (auto& it: regles) {
-			data.insert(std::pair<std::string, Regle>(it.nom, it));
-		}
-	}
-	BaseRegles::BaseRegles(const BaseRegles& br) : data(br.getData()) {}
-	BaseRegles& BaseRegles::operator=(const BaseRegles& br) {
-		data = br.getData();
-		return *this;
-	}
-	BaseRegles::BaseRegles(BaseRegles&& br) : data(br.getData()) {}
-	BaseRegles& BaseRegles::operator=(BaseRegles&& br) {
-		data = br.getData();
-		return *this;
-	}
-	BaseRegles::~BaseRegles() {}
-	void BaseRegles::addRegles(std::vector<Regle> regles) {
-		for (auto& it: regles) {
-			data.insert(std::pair<std::string, Regle>(it.nom, it));
-		}
-	}
-	void BaseRegles::rmRegle(std::string regle) {
-		data.erase(regle);
-	}
-	Regle BaseRegles::get(const std::string& nom) const {
-		return data.at(nom);
-	}
-	Regle BaseRegles::operator()(const std::string& nom) const {
-		return get(nom);
-	}
-	std::map<std::string, Regle> BaseRegles::getData() const {
-		return data;
-	}
-
-
-  BaseRegles::BaseRegles() {}
-  BaseRegles::BaseRegles(std::vector<std::shared_ptr<Regle> > regles) {
+  BaseRegles::BaseRegles(std::vector<std::shared_ptr<Regle> > regles) { //constructeur par tableau de règles
     for (auto& it: regles) {
       data.insert(std::pair<std::string, std::shared_ptr<Regle> >(it->nom, it));
     }
   }
-  BaseRegles::BaseRegles(const BaseRegles& br) {
-    data = br.getData();
-  }
-  BaseRegles& BaseRegles::operator=(const BaseRegles& br) {
-    data = br.getData();
-    return *this;
-  }
-  BaseRegles::~BaseRegles() {}
+
+
+
   void BaseRegles::addRegles(std::vector<std::shared_ptr<Regle> > regles) {
     for (auto& it: regles) {
       data.insert(std::pair<std::string, std::shared_ptr<Regle> >(it->nom, it));
     }
   }
-  void BaseRegles::rmRegle(std::string regle) {
-    data.erase(regle);
-  }
+
   std::shared_ptr<Regle> BaseRegles::get(const std::string& nom) const {
     return data.at(nom);
   }
@@ -116,90 +97,81 @@ namespace fuzzy {
     //à implémenter!!
     return true;
   }
-  Fait::Fait(std::string n, bool f, bool e, float c, std::string t) : nom(n), flou(f), eval(e), coeff(c), texte(t) {}
+
+  //// Faits//////
+  Fait::Fait(const std::string n, bool f, bool e, float c, std::string t) : nom(n), flou(f), eval(e), coeff(c), texte(t) {}
   Fait::~Fait() {}
 
-
+  
+  ///BaseFaits////
   BaseFaits::BaseFaits() {}
+
   BaseFaits::BaseFaits(std::vector<std::shared_ptr<Fait> > faits) {
     for (auto& it: faits) {
       data.insert(std::pair<std::string, std::shared_ptr<Fait> >(it->nom, it));
     }
   }
+
   BaseFaits::~BaseFaits() {}
+
   void BaseFaits::addFaits(std::vector<std::shared_ptr<Fait> > faits) {
     for (auto& it: faits) {
       data.insert(std::pair<std::string, std::shared_ptr<Fait> >(it->nom, it));
     }
   }
+
   void BaseFaits::rmFait(std::string fait) {
     data.erase(fait);
   }
+
   std::shared_ptr<Fait> BaseFaits::get(const std::string& nom) const {
     return data.at(nom);
   }
+
   std::shared_ptr<Fait> BaseFaits::operator()(const std::string& nom) const {
     return get(nom);
   }
-	BaseFaits::BaseFaits() {}
-	BaseFaits::BaseFaits(std::vector<Fait> faits) {
-		for (auto& it: faits) {
-			data.insert(std::pair<std::string, Fait>(it.nom, it));
-		}
-	}
-	BaseFaits::BaseFaits(const BaseFaits& bf) : data(bf.getData()) {}
-	BaseFaits& BaseFaits::operator=(const BaseFaits& bf) {
-		data = bf.getData();
-		return *this;
-	}
-	BaseFaits::BaseFaits(BaseFaits&& bf) : data(bf.getData()) {}
-	BaseFaits& BaseFaits::operator=(BaseFaits&& bf) {
-		data = bf.getData();
-		return *this;
-	}
-	BaseFaits::~BaseFaits() {}
-	void BaseFaits::addFaits(std::vector<Fait> faits) {
-		for (auto& it: faits) {
-			data.insert(std::pair<std::string, Fait>(it.nom, it));
-		}
-	}
-	void BaseFaits::rmFait(std::string fait) {
-		data.erase(fait);
-	}
-	Fait BaseFaits::get(const std::string& nom) const {
-		return data.at(nom);
-	}
-	Fait BaseFaits::operator()(const std::string& nom) const {
-		return get(nom);
-	}
-	std::map<std::string, Fait> BaseFaits::getData() const {
-		return data;
-	}
+
+ 
+  BaseFaits::BaseFaits(const BaseFaits& bf) : data(bf.getData()) {}
+
+  BaseFaits& BaseFaits::operator=(const BaseFaits& bf) {
+    data = bf.getData();
+    return *this;
+  }
+
+  BaseFaits::BaseFaits(BaseFaits&& bf) : data(bf.getData()) {}
+
+  BaseFaits& BaseFaits::operator=(BaseFaits&& bf) {
+    data = bf.getData();
+    return *this;
+  }
+
+  std::map<std::string, std::shared_ptr<Fait> > BaseFaits::getData() const {
+    return data;
+  }
 
 
 
-	std::ostream& operator<<(std::ostream& os, const BaseRegles& br) {
-		for (auto& it: br.data) {
-			os << it.second.nom << ": Si ";
+  // std::ostream& operator<<(std::ostream& os, const BaseRegles& br) {
+  //   for (auto& it: br.data) {
+  //     os << it->second.nom << ": Si ";
 			
-			for (auto& it_vec: it.second.prem) {
-				// os << "(" << it_vec.first->nom << ", ";
-				// os << it_vec.second->name << ")" << " et ";
-			}
-			os << " alors ";
-			// os << "(" << it.second.concl.first->nom; 
-			// os << ", " << it.second.concl.second->name;
-			os << ")" << std::endl;
-			os << "Flou: " << it.second.flou ? "true" : "false";
-			os << std::endl;
-			os << "Coeff: " << it.second.coeff << std::endl << std::endl; 
-		}
+  //     for (auto& it_vec: it->second->prem) {
+  // 	// os << "(" << it_vec.first->nom << ", ";
+  // 	// os << it_vec.second->name << ")" << " et ";
+  //     }
+  //     os << " alors ";
+  //     // os << "(" << it.second.concl.first->nom; 
+  //     // os << ", " << it.second.concl.second->name;
+  //     os << ")" << std::endl;
+  //     os << "Flou: " << it.second.flou ? "true" : "false";
+  //     os << std::endl;
+  //     os << "Coeff: " << it.second.coeff << std::endl << std::endl; 
+  //   }
 
-		return os; 
-	}	
-	std::ostream& operator<<(std::ostream& os, const BaseFaits& c);
-
-
-
-	
+  //   return os; 
+  // }	
+  // std::ostream& operator<<(std::ostream& os, const BaseFaits& c);
+  
 }
--- testParser.cpp
+++ testParser.cpp
@@ -11,4 +11,23 @@ int main( int argc, const char* argv[] )
   //std::cout<<br;
 
   fuzzy::Fait f1("niveau_math", true, false, 0, "décrit le niveau en math de l'étudiant");
+  fuzzy::Fait f2("niveau_physique", true, false, 0, "décrit le niveau en physique de l'étudiant");
+
+  std::vector<std::shared_ptr<fuzzy::Fait> > vfp;
+  std::vector<std::shared_ptr<fuzzy::Fait> > vfc;
+  vfp.push_back(std::make_shared<fuzzy::Fait>(f1));
+  vfc.push_back(std::make_shared<fuzzy::Fait>(f2));
+
+  fuzzy::BaseFaits bf(vfp);
+
+  fuzzy::Regle r1("R1", vfp, vfc, true, 0.9); 
+  fuzzy::Regle r2("R2", vfc, vfp, true, 0.3);
+
+  std::vector<std::shared_ptr<fuzzy::Regle> > vr;
+  vr.push_back(std::make_shared<fuzzy::Regle>(r1));
+  vr.push_back(std::make_shared<fuzzy::Regle>(r2));
+
+  fuzzy::BaseRegles br(vr);
+  
+  
 }
